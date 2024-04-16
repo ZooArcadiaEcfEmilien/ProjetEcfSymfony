70dd349b74fc636af1e68153d24435797f463b15
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-04-16 14:45:29 +0200

    test

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,39 +2,80 @@
 
 namespace App\Controller;
 
-use App\Entity\HabitatEntity;
-use App\Service\AnimalCrud;
+use App\Entity\AnimalEntity;
+use App\Form\AnimalEntityType;
+use App\Repository\AnimalEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-use App\Entity\AnimalEntity;
-
-
+#[Route('/animal')]
 class AnimalController extends AbstractController
 {
-    #[Route('/animal', name: 'app_animal')]
-    public function index(): Response
+    #[Route('/', name: 'app_animal_index', methods: ['GET'])]
+    public function index(AnimalEntityRepository $animalEntityRepository): Response
     {
-        return $this->render('animal/animal.html.twig', [
-            'controller_name' => 'AnimalController',
+        return $this->render('animal/index.html.twig', [
+            'animal_entities' => $animalEntityRepository->findAll(),
         ]);
     }
-    private $animalCrud;
 
-    public function __construct(AnimalCrud $animalCrud)
+    #[Route('/new', name: 'app_animal_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $this->animalCrud = $animalCrud;
+        $animalEntity = new AnimalEntity();
+        $form = $this->createForm(AnimalEntityType::class, $animalEntity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($animalEntity);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('animal/new.html.twig', [
+            'animal_entity' => $animalEntity,
+            'form' => $form,
+        ]);
     }
 
-    #[Route('/animal/add', name: 'animal_add')]
-    public function addAnimal(string $name, string $race, HabitatEntity $habitat, $dateTime, string $detailsCommentaire, string $etatAnimal, int $nourritureQuantite, string $nourritureType
-    ): Response {
-        $this->animalCrud->addAnimal($name, $race, $habitat, $dateTime, $detailsCommentaire, $etatAnimal, $nourritureQuantite, $nourritureType);
+    #[Route('/{id}', name: 'app_animal_show', methods: ['GET'])]
+    public function show(AnimalEntity $animalEntity): Response
+    {
+        return $this->render('animal/show.html.twig', [
+            'animal_entity' => $animalEntity,
+        ]);
+    }
 
-        return new Response('Animal ajouté avec succès !');
+    #[Route('/{id}/edit', name: 'app_animal_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, AnimalEntity $animalEntity, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AnimalEntityType::class, $animalEntity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('animal/edit.html.twig', [
+            'animal_entity' => $animalEntity,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_animal_delete', methods: ['POST'])]
+    public function delete(Request $request, AnimalEntity $animalEntity, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$animalEntity->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($animalEntity);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
     }
 }
-
-
-
--- src/Entity/ServiceTabEntity.php
+++ src/Entity/ServiceTabEntity.php
@@ -29,32 +29,4 @@ class ServiceTabEntity
     {
         return $this->id;
     }
-    /*public function createProperty($propertyName, $propertyValue){
-        $setterMethod = 'set' . ucfirst($propertyName);
-        if (method_exists($this, $setterMethod)) {
-            $this->$setterMethod($propertyValue);
-        }
-    }
-
-    public function readProperty($propertyName){
-        $getterMethod = 'get' . ucfirst($propertyName);
-        if (method_exists($this, $getterMethod)) {
-            return $this->$getterMethod();
-        }
-        return null;
-    }
-
-    public function updateProperty($propertyName, $propertyValue){
-        $setterMethod = 'set' . ucfirst($propertyName);
-        if (method_exists($this, $setterMethod)) {
-            $this->$setterMethod($propertyValue);
-        }
-    }
-
-    public function deleteProperty($propertyName){
-        $setterMethod = 'set' . ucfirst($propertyName);
-        if (method_exists($this, $setterMethod)) {
-            $this->$setterMethod(null);
-        }
-    }*/
 }

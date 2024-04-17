9ad608c900695f2b4ddc9d110b7ccdf94bb3351e
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-04-17 15:00:35 +0200

    CorrectifAffichageEasyAdmin

--- src/Controller/Admin/AnimalEntityCrudController.php
+++ src/Controller/Admin/AnimalEntityCrudController.php
@@ -4,9 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\AnimalEntity;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Fieldtext;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
 class AnimalEntityCrudController extends AbstractCrudController
 {
@@ -15,14 +19,21 @@ class AnimalEntityCrudController extends AbstractCrudController
         return AnimalEntity::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('race'),
+            TextEditorField::new('image'),
+            TextField::new('habitat'),
+            TextField::new('etatAnimal'),
+            TextField::new('nourritureType'),
+            IntegerField::new('nourritureQuantite'),
+            DateTimeField::new('datePassage'),
+            TextField::new('detailsCommentaire'),
+
         ];
     }
-    */
+    
 }
--- src/Entity/UserTabEntity.php
+++ src/Entity/UserTabEntity.php
@@ -26,38 +26,26 @@ class UserTabEntity
 
     #[ORM\Column(type:"string")]
     private $mail;
+
     public function getId(): ?int
     {
         return $this->id;
     }
-    /*
-    public function createProperty($propertyName, $propertyValue){
-        $setterMethod = 'set' . ucfirst($propertyName);
-        if (method_exists($this, $setterMethod)) {
-            $this->$setterMethod($propertyValue);
-        }
+    public function getUserType(): ?string
+    {
+        return $this->userType;
+    }
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+    public function getUserName(): ?string
+    {
+        return $this->userName;
     }
 
-    public function readProperty($propertyName){
-        $getterMethod = 'get' . ucfirst($propertyName);
-        if (method_exists($this, $getterMethod)) {
-            return $this->$getterMethod();
-        }
-        return null;
-    }
-
-    public function updateProperty($propertyName, $propertyValue){
-        $setterMethod = 'set' . ucfirst($propertyName);
-        if (method_exists($this, $setterMethod)) {
-            $this->$setterMethod($propertyValue);
-        }
-    }
-
-    public function deleteProperty($propertyName){
-        $setterMethod = 'set' . ucfirst($propertyName);
-        if (method_exists($this, $setterMethod)) {
-            $this->$setterMethod(null);
-        }
-    }
-    */
 }

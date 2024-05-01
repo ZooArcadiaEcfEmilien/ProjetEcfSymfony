af1d0a3ce46c9fc62a0867d4d878342bdcbe166d
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-01 15:58:03 +0200

    Avis fonctionnel
    Avis Crud fonctionnel intra/utilisateur
    Reste à finaliser Front/ui

--- src/Form/AvisEntityType.php
+++ src/Form/AvisEntityType.php
@@ -6,19 +6,24 @@ use App\Entity\AvisEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AvisEntityType extends AbstractType
 {
-   public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombreEtoileAvis')
-            ->add('pseudoAvis')
-            ->add('descriptionAvis')
+            ->add('nombreEtoileAvis', IntegerType::class)
+            ->add('pseudoAvis', TextType::class)
+            ->add('descriptionAvis', TextareaType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Ajouter mon avis'])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => AvisEntity::class,

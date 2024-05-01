5af26cbae30539252f84f3843a4ea3bf4963c429
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-01 12:30:39 +0200

    config intra 80% ok
    fonctionnel, ajout image avec bdd/intra ok

--- src/Controller/Admin/FormulaireEntityCrudController.php
+++ src/Controller/Admin/FormulaireEntityCrudController.php
@@ -8,10 +8,19 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
-
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class FormulaireEntityCrudController extends AbstractCrudController
 {
+    public function configureActions(Actions $actions): Actions{
+
+        $actions->add(Crud::PAGE_INDEX, Action::DETAIL)
+                ->disable(Action::NEW,Action::DELETE, Action::EDIT);
+
+        return $actions;
+    }
     public static function getEntityFqcn(): string
     {
         return FormulaireEntity::class;
--- src/Controller/Admin/UserTabEntityCrudController.php
+++ src/Controller/Admin/UserTabEntityCrudController.php
@@ -6,26 +6,45 @@ use App\Entity\UserTabEntity;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class UserTabEntityCrudController extends AbstractCrudController
-{
+{    
     public static function getEntityFqcn(): string
     {
         return UserTabEntity::class;
     }
+    public function configureActions(Actions $actions): Actions{
 
-    
+        $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+        return $actions;
+    }
     public function configureFields(string $pageName): iterable
-{
-    return [
-        TextField::new('userType'),
-        TextField::new('password')
-            ->setFormType(PasswordType::class)
-          /*  ->onlyOnForms()*/,                    // A dé commenter une fois que le mdp sera sécurisé partout et testé / 
-        TextField::new('userName'),
-        EmailField::new('mail'),
-    ];
-}
-    
+    {
+        return [
+            ChoiceField::new('userType')
+            ->setChoices([
+                'Employé' => 'option1',
+                'Vétérinaire' => 'option2',
+                'Intérimaire/Stagiaire' => 'option3',
+                'Admin' => 'option4',
+
+            ])
+            ->setFormType(ChoiceType::class),            
+            
+            TextField::new('password')
+                ->setFormType(PasswordType::class),
+            TextField::new('userName'),
+            EmailField::new('mail'),
+        ];
+    }
 }

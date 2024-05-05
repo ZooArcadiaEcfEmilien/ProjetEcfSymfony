6722de34dc233f2487d453978b272b18f7918a88
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-05 17:51:03 +0200

    Front
    Accueil front + Avis ok

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ServiceTabEntityRepository;
 
 class ServiceController extends AbstractController
 {
     #[Route('/service', name: 'app_service')]
-    public function index(): Response
+    public function index(ServiceTabEntityRepository $serviceRepository): Response
     {
+        $services = $serviceRepository->findAll();
+
         return $this->render('Service.html.twig', [
-            'controller_name' => 'ServiceController',
+            'services' => $services,
         ]);
     }
 }

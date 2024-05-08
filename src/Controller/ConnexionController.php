8cf81c2669b26734f6a188aaf997626f8780ac15
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-08 22:28:44 +0200

    firstcommit

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class ConnexionController extends AbstractController
 {
     #[Route('/connexion', name: 'app_connexion')]
-    public function index(): Response
-    {
+        public function index(AuthenticationUtils $authenticationUtils): Response    
+        {
+            $error = $authenticationUtils->getLastAuthenticationError();
+            $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('Connexion.html.twig', [
-            'controller_name' => 'ConnexionController',
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
     }
 }

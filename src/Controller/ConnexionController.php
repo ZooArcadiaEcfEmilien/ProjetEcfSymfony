bfc82b17e1641851ed6a087de51de9dd89b256a8
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-09 16:55:00 +0200

    CREATION USER OK

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\UserTabEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class ConnexionController extends AbstractController
 {

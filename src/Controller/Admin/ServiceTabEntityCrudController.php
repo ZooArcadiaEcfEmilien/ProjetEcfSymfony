95249ec92dd37302f32551b08434bc80ae39daf7
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-13 15:36:46 +0200

    ROLE OK

--- src/Controller/Admin/ServiceTabEntityCrudController.php
+++ src/Controller/Admin/ServiceTabEntityCrudController.php
@@ -10,15 +10,24 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 
 class ServiceTabEntityCrudController extends AbstractCrudController
 {
-    public function configureActions(Actions $actions): Actions{
-
+    public function index(AdminContext $context)
+    {
+        if (!$this->isGranted('ROLE_ADMIN') && !$this->isGranted('ROLE_EMPLOYE')) {
+            return $this->redirectToRoute('admin');
+        }
+        // Continuer avec l'affichage de la page normalement
+        return parent::index($context);
+    }
+    public function configureActions(Actions $actions): Actions
+    {
         $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
         return $actions;
     }
+
     public static function getEntityFqcn(): string
     {
         return ServiceTabEntity::class;
@@ -27,10 +36,12 @@ class ServiceTabEntityCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('serviceNom','Nom du service'),
+            TextField::new('serviceNom', 'Nom du service'),
             TextField::new('serviceTitre', 'Titre choisi pour le site'),
             TextEditorField::new('serviceDescription', 'Description du service'),
             ImageField::new('serviceImage', 'Insérer une image')->setUploadDir('/public/uploads/images/Services'),
         ];
     }
+
+
 }
--- src/Controller/Admin/StatistiqueEntityCrudController.php
+++ src/Controller/Admin/StatistiqueEntityCrudController.php
@@ -7,22 +7,25 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 
 class StatistiqueEntityCrudController extends AbstractCrudController
 {
+    public function index(AdminContext $context)
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin');
+        }
+        // Continuer avec l'affichage de la page normalement
+        return parent::index($context);
+    }
+
+
     public static function getEntityFqcn(): string
     {
         return StatistiqueEntity::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
 }

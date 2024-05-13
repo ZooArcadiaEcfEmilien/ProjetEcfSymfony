f057c42761a959211cbc8f22bae32ba415c5c72b
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-05-13 21:50:38 +0200

    ok

--- src/Service/AnimalCrud.php
+++ src/Service/AnimalCrud.php
@@ -13,22 +13,6 @@ class AnimalCrud
     {
         $this->entityManager = $entityManager;
     }
-
-  /*  public function addAnimal(string $name, string $race, HabitatEntity $habitat, \DateTime $dateTime, ?String $detailsCommentaire, string $etatAnimal, int $nourritureQuantite, string $nourritureType): void
-    {
-        $animal = new AnimalEntity();
-        $animal->setName($name);
-        $animal->setRace($race);
-        $animal->setHabitatNom($habitat);
-        $animal->setdatePassage($dateTime);
-        $animal->setDetailsCommentaire($detailsCommentaire);
-        $animal->setEtatAnimal($etatAnimal);
-        $animal->setnourritureQuantite($nourritureQuantite);
-        $animal->setnourritureType($nourritureType);
-
-        $this->entityManager->persist($animal);
-        $this->entityManager->flush();
-    }*/
 }
 
 

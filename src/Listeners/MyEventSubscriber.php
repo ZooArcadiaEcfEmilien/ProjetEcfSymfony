7a001ac4a840c10c75013bf372a7b270f8625f32
ProjetEcfSymfony <157069062+ZooArcadiaEcfEmilien@users.noreply.github.com>
2024-07-15 11:07:29 +0200

    Connection bdd ok + fix bug précédent
    Connection a la bdd ok via test playground, fix du bug précédent sur l'affichage du site

--- src/Listeners/MyEventSubscriber.php
+++ src/Listeners/MyEventSubscriber.php
@@ -6,8 +6,43 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
 use App\Entity\Order;
 use Documents\Product;
+
 class MyEventSubscriber
 {
+    private $dm;
+
+    public function __construct(DocumentManager $dm)
+    {
+        $this->dm = $dm;
+    }
+
+    public function postLoad(LifecycleEventArgs $eventArgs): void
+    {
+        $order = $eventArgs->getDocument();
+
+        if (!$order instanceof Order) {
+            return;
+        }
+
+        $productReflProp = $this->dm->getClassMetadata(Order::class)
+            ->reflClass->getProperty('product');
+        $productReflProp->setAccessible(true);
+        $productReflProp->setValue(
+            $order, $this->dm->getReference(Product::class, $order->getProductId())
+        );
+    }
+}
+/* <?php
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+use Documents\Product;
+use App\Entity\Order;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+class MyEventSubscriber
+{
+    private $dm;
+
     public function __construct(DocumentManager $dm)
     {
         $this->dm = $dm;
@@ -21,12 +56,13 @@ class MyEventSubscriber
             return;
         }
 
-        $dm = $eventArgs->getDocumentManager();
-        $productReflProp = $dm->getClassMetadata(Order::class)
-            ->reflClass->getProperty('product');
+        $em = $eventArgs->getObjectManager();
+        $productReflProp = $em->getClassMetadata(Order::class)
+            ->getReflectionClass()->getProperty('product');
         $productReflProp->setAccessible(true);
         $productReflProp->setValue(
-            $order->$this->dm->getReference(Product::class, $order->getProductId())
+            $order, $this->dm->getReference(Product::class, $order->getProductId())
         );
     }
 }
+    */

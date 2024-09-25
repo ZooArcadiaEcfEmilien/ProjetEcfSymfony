6410f1bf252dd72b5665b837e5254a37d0030463
Emilien Duplaquet <emilien.duplaquet@studi.fr>
2024-09-25 18:14:43 +0200

    minor fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,11 @@
 <?php
 
-use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-// Ne charger .env que si APP_ENV n'est pas défini ou est en développement
-$dotenv = new Dotenv();
-if (empty($_SERVER['APP_ENV']) || $_SERVER['APP_ENV'] === 'dev') {
-    $dotenv->load(dirname(__DIR__) . '/.env');
+if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    require dirname(__DIR__).'/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
